190c364cdaa8a22c68ed075c9891a47c8cd3feb9
Sheryl Phoa <sherylphoa18@gmail.com>
2026-01-31 12:06:36 +0800

    Add LocalDate to Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that must be completed by a specified deadline.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     /**
+     * Constructs a Deadline task.
+     *
      * @param description The description of the deadline.
      * @param by The date/time the task is due.
      */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
