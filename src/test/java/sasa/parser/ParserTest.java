a6cf0bec45052b120f13a078da4ca697916fe0af
Sheryl Phoa <sherylphoa18@gmail.com>
2026-02-16 22:35:11 +0800

    Add more JUnit tests

--- src/test/java/sasa/parser/ParserTest.java
+++ src/test/java/sasa/parser/ParserTest.java
@@ -1,15 +1,17 @@
 package sasa.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import sasa.commands.AddCommand;
+import sasa.commands.ExitCommand;
 import sasa.exception.SasaException;
 
 public class ParserTest {
     @Test
     public void parseIndex_validNumber_success() throws SasaException {
-        // User inputs "5", we expect index 4
         assertEquals(4, Parser.parseIndex("5"));
     }
 
@@ -18,5 +20,15 @@ public class ParserTest {
         assertThrows(SasaException.class, () -> {
             Parser.parseIndex("abc");
         });
+    }
+
+    @Test
+    public void parse_todoCommand_returnAddCommand() throws SasaException {
+        assertTrue(Parser.parse("todo borrow book") instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_byeCommand_returnExitCommand() throws SasaException {
+        assertTrue(Parser.parse("bye") instanceof ExitCommand);
     }
 }
